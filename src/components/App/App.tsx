6a47d358269a0f18c103b5ecbb64d72cc8066ad1
tarcisio-vieira <tarcisio@outlook.com.br>
2023-11-25 15:23:27 -0300

    Commit init

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestComponent from '../TestComponent'
+import ClassComponent from '../ClassComponent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <TestComponent />
+        <ClassComponent name="Mundo" />
         <a
           className="App-link"
           href="https://reactjs.org"
